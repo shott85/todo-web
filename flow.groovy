adfeddea54ff4ff927739b35f703d44423828872
Andy Pemberton <pembertona@gmail.com>
2015-05-29 11:35:22 -0400

    wrap build and int test in node

--- flow.groovy
+++ flow.groovy
@@ -1,16 +1,17 @@
-stage 'build' 
+node('jdk7') {
 
-	node('jdk7') {
+	stage 'build'
+
+		checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/apemberton/todo-web.git']]])
 		env.PATH="${tool 'mvn-3.2.2'}/bin:${env.PATH}"
 		sh 'mvn clean package'
-		archive 'target/*.war'
-	}
+		archive pom.xml
+		archive 'target/**'
 
-stage 'integration-test' 
+	stage 'integration-test' 
 
-	node('jdk7') {
 		sh 'mvn verify'
-	}
+}
 
 stage 'quality-and-functional-test'
 
