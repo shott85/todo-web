8c2aae079d0cebf3e65dbc1e43854f3158c7ce77
Andy Pemberton <pembertona@gmail.com>
2015-05-29 11:42:05 -0400

    add an input step

--- flow.groovy
+++ flow.groovy
@@ -1,14 +1,12 @@
 node('jdk7') {
 
 	stage 'build'
-
 		checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/apemberton/todo-web.git']]])
 		env.PATH="${tool 'mvn-3.2.2'}/bin:${env.PATH}"
 		sh 'mvn clean package'
 		archive 'target/*.war'
 
 	stage 'integration-test' 
-
 		sh 'mvn verify'
 }
 
@@ -23,6 +21,16 @@ stage 'quality-and-functional-test'
     	echo 'selenium test'
         // build 'sauce-labs-test'
     })
+
+    try {
+        checkpoint('Testing Complete')
+    } catch (NoSuchMethodError _) {
+        echo 'Checkpoint feature available in Jenkins Enterprise by CloudBees.'
+    }
+
+
+stage 'approval'
+	input 'Do you approve deployment to production?'
 
 stage 'production'
 	echo 'mvn cargo:deploy'
