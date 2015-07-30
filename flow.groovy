7df45dafddf837d0b6625291d4c98694fd433e62
David Schott <schottdb@gmail.com>
2015-07-30 14:44:35 -0400

    Update flow.groovy

--- flow.groovy
+++ flow.groovy
@@ -2,7 +2,7 @@ node('linux') {
 
 	stage 'build'
 		checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/apemberton/todo-web.git']]])
-		env.PATH="${tool 'mvn-3.2.2'}/bin:${env.PATH}"
+		env.PATH="${tool 'M3'}/bin:${env.PATH}"
 		sh 'mvn clean package'
 		archive 'target/*.war'
 
